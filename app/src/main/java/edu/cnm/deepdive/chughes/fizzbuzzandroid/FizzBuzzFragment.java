0944409c8b84f2a38b88400afb910a5269c55e73
Chris Hughes <chris@speedycomputing.com>
2018-05-29 14:38:09 -0600

    New Fragment and switching between fragments

---
+++ app/src/main/java/edu/cnm/deepdive/chughes/fizzbuzzandroid/FizzBuzzFragment.java
@@ -0,0 +1,144 @@
+package edu.cnm.deepdive.chughes.fizzbuzzandroid;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FizzBuzzFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FizzBuzzFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FizzBuzzFragment extends Fragment implements TextWatcher {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private TextView textView;
+
+    private OnFragmentInteractionListener mListener;
+
+    public FizzBuzzFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FizzBuzzFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FizzBuzzFragment newInstance(String param1, String param2) {
+        FizzBuzzFragment fragment = new FizzBuzzFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fizz_buzz, container, false);
+
+        textView = view.findViewById(R.id.fizzbuzztext);
+
+        EditText input = view.findViewById(R.id.editText);
+
+        input.addTextChangedListener(this);
+
+        updateText(10);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void updateText(int count_to) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < count_to; i++) {
+            stringBuilder.append(FizzBuzz.count(i+1)+"\n");
+        }
+
+        textView.setText(stringBuilder);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        try {
+            updateText(Integer.parseInt(editable.toString()));
+        } catch (NumberFormatException e){
+            Toast.makeText(getActivity(),"Invalid Input",Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/edu/cnm/deepdive/chughes/fizzbuzzandroid/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/chughes/fizzbuzzandroid/MainActivity.java
@@ -1,58 +1,43 @@
 package edu.cnm.deepdive.chughes.fizzbuzzandroid;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private FrameLayout frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.fizzbuzztext);
+        frame = findViewById(R.id.fragment);
 
-        EditText input = findViewById(R.id.editText);
-
-        input.addTextChangedListener(this);
-
-        updateText(10);
+        showFizzBuzz(null);
 
     }
 
-    private void updateText(int count_to) {
-        StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 0; i < count_to; i++) {
-            stringBuilder.append(FizzBuzz.count(i+1)+"\n");
-        }
-
-        textView.setText(stringBuilder);
+    public void showFizzBuzz(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment, new FizzBuzzFragment())
+                .commit();
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable editable) {
-        try {
-            updateText(Integer.parseInt(editable.toString()));
-        } catch (NumberFormatException e){
-            Toast.makeText(this,"Invalid Input",Toast.LENGTH_LONG).show();
-        }
+    public void showSecond(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment, new SecondFragment())
+                .commit();
     }
 }
